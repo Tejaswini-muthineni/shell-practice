7e7f216317e1364250d244200fb8dc4af36efbbe
tejaswini <muthinenitejaswini9@gmail.com>
2025-10-16 09:33:20 +0530

    conversation code

--- conversation.sh
+++ conversation.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-read -s PIN
-echo "pin is : $PIN"
-P1=$1
+
 echo "$P1 : Hi ABhi, how are you doing?"
 echo "Abhi: HI $P1 , I'm doing good , what are you doing here?"
 echo "$P1: I came here meet my cousin abhi"
