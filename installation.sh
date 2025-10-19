fd5b308996937feade3f69311c3b9dc4a6057b58
tejaswini <muthinenitejaswini9@gmail.com>
2025-10-19 12:48:23 +0530

    sql installation

--- installation.sh
+++ installation.sh
@@ -6,10 +6,11 @@ if [ $USER_ID -eq 0 ]; then
     echo "user has root access"
 fi
 
-dnf install mysql -y
+echo "$?"
+#dnf install mysql -y
 
-if [ $? -ne 0 ]; then
-    echo "dnf installation failed"
-else
-    echo "installation sucess"
-fi
+#if [ $? -ne 0 ]; then
+#   echo "dnf installation failed"
+#else
+#    echo "installation sucess"
+#fi
