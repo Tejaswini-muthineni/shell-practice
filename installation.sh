6d762c19dbba8af6abe89cb00557789eca8af676
tejaswini <muthinenitejaswini9@gmail.com>
2025-10-19 12:51:52 +0530

    sql installation

--- installation.sh
+++ installation.sh
@@ -5,9 +5,11 @@ USER_ID=$(id -u)
 if [ $USER_ID -eq 0 ]; then
     echo "user has root access"
     echo " id -u is $USER_ID "
+else 
+    echo " no root access"
 fi
 
-echo "$?"
+
 #dnf install mysql -y
 
 #if [ $? -ne 0 ]; then
