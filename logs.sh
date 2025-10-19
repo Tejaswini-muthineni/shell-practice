7117432e6f852aa93c7117478af957ae9723eec1
tejaswini <muthinenitejaswini9@gmail.com>
2025-10-19 18:01:30 +0530

    logs practivce

--- logs.sh
+++ logs.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+LOGS_FOLDER="/var/log/shell-script"
 echo $0 | cut -d "." -f1
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+N="\e[0m"
+
+RED="\e[31m"
+
+echo -e " log file is $RED $LOGFILE $N"
