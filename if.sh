44bbba78e77df398642b4a4584ce5ec89eeb9447
tejaswini <muthinenitejaswini9@gmail.com>
2025-10-19 12:18:34 +0530

    variables

--- if.sh
+++ if.sh
@@ -4,4 +4,9 @@ read NUM
 echo " number is $NUM "
 if [ $NUM -lt 10 ]; then
     echo " num less than 10 "
+elif [ $NUM -eq 10]; then
+    echo "num is 10" 
+else 
+    echo "number is greater than 10"
 fi
+
