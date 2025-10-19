fb2ad47cd8c371d89dac3647463362c6abee166e
tejaswini <muthinenitejaswini9@gmail.com>
2025-10-19 12:09:11 +0530

    variables

--- variables.sh
+++ variables.sh
@@ -5,6 +5,6 @@ NUMBER2=$2
 SUM=$(($NUMBER1+$NUMBER2))
 echo "sum is : ${SUM} "
 
-NAME=("teja" "tanu" "yesh")
+NAMES=("teja" "tanu" "yesh")
 echo "all leaders are ${NAMES[@]}"
 echo "name at 0 index is : ${NAMES[0]}"
