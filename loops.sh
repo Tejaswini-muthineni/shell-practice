3fddf857def79c211aecd0dd7657904ffbd3b163
tejaswini <muthinenitejaswini9@gmail.com>
2025-10-20 08:04:30 +0530

    loops practice

---
+++ loops.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+USER=$( id -u )
+G="\e[32m"
+R="\e[31m"
+N="\e[0m"
+
+if [ $USER -ne 0 ]; then
+    echo " take root user access"
+    exit 1
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+    echo "mysql installation.... $R failed $N"
+else 
+    echo "my sql installation...$G success $N"
